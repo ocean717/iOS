35c12e8dd923eec2d1743ce84f55ecbdbccad978
HY <798496849@qq.com>
2017-02-07 15:25:40 +0800

    test

---
+++ CGQWebView/CGQWeb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGQWeb
+//
+//  Created by 中科天地 on 16/9/26.
+//  Copyright © 2016年 wangchengshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CGQWebView/CGQWeb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CGQWeb
+//
+//  Created by 中科天地 on 16/9/26.
+//  Copyright © 2016年 wangchengshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
